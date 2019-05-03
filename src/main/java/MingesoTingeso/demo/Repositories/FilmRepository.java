4dff1350bcff5a3e3ce6c3f6be45a548c36d4293
Edgar Blau <edgar.blau@usach.cl>
2019-05-03 12:52:51 -0400

    Permite CREATE y GET desde la DB.

--- src/main/java/MingesoTingeso/demo/Repositories/FilmRepository.java
+++
@@ -1,8 +0,0 @@
-package MingesoTingeso.demo.Repositories;
-
-import MingesoTingeso.demo.Models.Film;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FilmRepository extends JpaRepository<Film, Long>{
-    Film findFilmsById(Long id);
-}
---
+++ src/main/java/MingesoTingeso/demo/Repositories/ProductoRepository.java
@@ -0,0 +1,14 @@
+package MingesoTingeso.demo.Repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import MingesoTingeso.demo.Models.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
+	Producto findProductoByCodigoProducto(int codigoProducto);
+	
+	Producto findProductoById(Long id);
+	
+	Producto findProductoByCategoria(String categoria);
+	
+	Producto findProductoByNombreProducto(String nombreProducto);
+}
