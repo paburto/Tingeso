85a616eba8b3a89c3d127efb9f5ea62a17b0d0d0
josemellado <jose.mellado@usach.cl>
2019-05-07 01:36:51 -0400

    junit 2

--- src/test/java/MingesoTingeso/demo/DemoApplicationTests.java
+++ src/test/java/MingesoTingeso/demo/DemoApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.*;
-import main.java.MingesoTingeso.demo.Models.*;
+import main.java.MingesoTingeso.demo.Models.Producto;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
